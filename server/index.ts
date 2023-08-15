8447611472c0ffebf9ee209c7752d6508f9cd5e3
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-08-15 19:58:58 +0700

    add single page post (pre-rendered page)

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { Post } from '../shared/types';
 
 const categories = require("./categories.json");
 const posts = require("./posts.json");
@@ -19,6 +20,13 @@ app.get("/posts", (_, res) => {
 // tampilkan semua data categories
 app.get("/categories", (_, res) => {
     return res.json(categories);
+});
+
+// tampilkan single data post
+app.get("/posts/:id", (req, res) => {
+    const postId = String(req.params.id);
+    const post = posts.find(({ id }: Post) => String(id) === postId);
+    return res.json(post);
 });
 
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}!`));
