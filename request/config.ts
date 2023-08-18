52191a858445ec3d723eb2907e6129500af623c4
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-08-18 23:06:33 +0700

    remove host "localhost:3000"

--- request/config.ts
+++ request/config.ts
@@ -1,6 +1,6 @@
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 const protocol = IS_PRODUCTION ? "https" : "http";
-const host = process.env.NEXT_PUBLIC_VERCEL_URL || "localhost:3000";
+const host = process.env.NEXT_PUBLIC_VERCEL_URL;
 
 export const config = {
     baseUrl: `${protocol}://${host}/api`
