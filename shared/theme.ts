2641621028e956d8f55b49e1f420d05b3f6ac79d
Arief Kurniawan <78076941+arief-github@users.noreply.github.com>
2023-08-18 22:22:16 +0700

    fix bug reference

--- shared/theme.ts
+++ shared/theme.ts
@@ -32,7 +32,7 @@ export const GlobalStyle = createGlobalStyle<MainThemeProps>`
     }
     
     a {
-      color: ${(theme) => theme.colors?.blue};
+      color: ${({theme}) => theme.colors?.blue};
     }
     
     a:hover {
